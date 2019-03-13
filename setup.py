169ee9798e34c475a165565ea327df6ec964440d
Michael Lyons <mdl0394@gmail.com>
2019-03-12 21:30:56 -0700

    Added url

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     description='Complicated python api. Control Apple Watch Complications with WebHooks',
     long_description=README,
     long_description_content_type="text/markdown",
+    url="https://github.com/frenchie4111/complicated-python",
     author='Mike Lyons',
     author_email='mdl0394@gmail.com',
     packages=find_packages( exclude=[ 'tmp' ] ),
