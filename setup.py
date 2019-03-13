647a18534ec9af971841cc2a962b716316d1ddd9
Michael Lyons <mdl0394@gmail.com>
2019-03-12 21:28:20 -0700

    Added more setup

--- setup.py
+++ setup.py
@@ -2,10 +2,18 @@
 
 from setuptools import setup, find_packages
 
+# The directory containing this file
+HERE = pathlib.Path( __file__ ).parent
+
+# The text of the README file
+README = ( HERE / "Readme.md" ).read_text()
+
 setup(
     name='complicated',
-    version='1.0',
+    version='1.0.0',
     description='Complicated python api. Control Apple Watch Complications with WebHooks',
+    long_description=README,
+    long_description_content_type="text/markdown",
     author='Mike Lyons',
     author_email='mdl0394@gmail.com',
     packages=find_packages( exclude=[ 'tmp' ] ),
@@ -14,5 +22,6 @@ setup(
     ],
     install_requires=[
         'requests'
-    ]
+    ],
+    license="MIT",
 )
